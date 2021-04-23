b3febdd818b0e3b4f71175770b0b412f68fad6be
Nehagoyal2008 <81558767+Nehagoyal2008@users.noreply.github.com>
2021-04-23 17:31:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG,helicopter;
+var packageBody,ground;
+var rect1, rect2, rect3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+rect1 = createSprite(500,620,15,80);
+rect1.shapeColor="red";
+
+rect2 = createSprite(300,620,15,80);
+rect2.shapeColor="red";
+
+rect3 = createSprite(400,655,186,15);
+rect3.shapeColor="red";
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	 Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+packageSprite.collide(rect1);
+packageSprite.collide(rect2);
+packageSprite.collide(rect3);
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+if(keyCode === LEFT_ARROW){
+	helicopterSprite.x=helicopterSprite.x-20;
+translation={x:-20,y:0}
+Matter.Body.translate(packageBody,translation)
+
+}else if (keyCode === RIGHT_ARROW){
+	helicopterSprite.x=helicopterSprite.x+20;
+translation={x:0,y:0}
+Matter.Body.translate(packageBody,translation)
+}
+
+ if (keyCode === DOWN_ARROW) {
+   Matter.Body.setStatic(packageBody,false);
+
+  }
+
+}
